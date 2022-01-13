d3889e971fef147251c5eb29f0f7120d50d1e3d3
Oukki <osiljamo@gmail.com>
2022-01-13 02:17:06 +0200

    Update to use config file

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import argparse
+import configparser
+import smtplib
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 
 from markdown import markdown
 from markdown.extensions.toc import TocExtension
@@ -8,7 +14,7 @@ from premailer import transform
 
 
 def create_html(output_file: str = "output.html", template_file: str = "template.html",
-                markdown_file: str = "example.md") -> None:
+                markdown_file: str = "example.md") -> (str, str, str):
     # Read template file to memory and convert to soup
     with open(template_file, "r") as f:
         template = f.read()
@@ -27,7 +33,8 @@ def create_html(output_file: str = "output.html", template_file: str = "template
     content_div.contents = content_soup
 
     # Set title
-    template_soup.title.string = content_soup.find("h1").string
+    title = content_soup.find("h1").string
+    template_soup.title.string = title
 
     # Change back to string and make css inline
     output_html = template_soup.prettify()
@@ -37,15 +44,49 @@ def create_html(output_file: str = "output.html", template_file: str = "template
     with open(f"output/{output_file}", "w", encoding="utf-8", errors="xmlcharrefreplace") as output_file:
         output_file.write(output_html)
 
+    return output_html, content, title
+
+
+def send_email(from_field: str, to_field: str, subject: str, html_content: str, text_content: str,
+               smtp_server: str, port: int, user: str, pwd: str):
+    msg = MIMEMultipart('alternative')
+    msg["Subject"] = subject
+    msg["From"] = from_field
+    msg["To"] = to_field
+
+    text = text_content
+    html = html_content
+
+    text_mime = MIMEText(text, 'plain')
+    html_mime = MIMEText(html, 'html')
+
+    msg.attach(text_mime)
+    msg.attach(html_mime)
+
+    mail = smtplib.SMTP(smtp_server, port)
+    mail.starttls()
+    mail.login(user, pwd)
+    mail.sendmail(from_field, to_field, msg.as_string())
+    mail.quit()
+
 
 def main():
     parser = argparse.ArgumentParser(description="InkuMail2 newsletter generator")
-    parser.add_argument("-o", "--output", type=str, help="Output file name", default="viikkomaili.html")
-    parser.add_argument("-t", "--template", type=str, help="Template file name", default="template.html")
-    parser.add_argument("-f", "--file", type=str, help="Markdown content file", default="example.md")
+    parser.add_argument("--dry-run", action="store_true", help="Only generate html file. Do not send email")
 
     args = parser.parse_args()
-    create_html(output_file=args.output, template_file=args.template, markdown_file=args.file)
+
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    output_html, markdown_content, title = create_html(output_file=config["newsletter"]["output"], template_file=config["newsletter"]["template"], markdown_file=config["newsletter"]["content"])
+
+    if args.dry_run:
+        return
+
+    send_email(from_field=config["email"]["from"], to_field=config["email"]["to"], subject=title,
+               html_content=output_html, text_content=markdown_content, smtp_server=config["email"]["smtp_server"],
+               port=int(config["email"]["port"]), user=config["email"]["username"], pwd=config["email"]["password"])
 
 
 if __name__ == "__main__":
